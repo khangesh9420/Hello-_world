d2be40984bba74618f70b68cb062ced71459bc6a
root <khangeshmatte@gmail.com>
2025-05-21 10:50:30 +0200

    my first task

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Preprocessor directive to include input-output stream
+
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Return 0 to indicate that the program ended successfully
+}
+
